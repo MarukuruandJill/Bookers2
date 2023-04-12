3113904637e872e27b63aba4e8c9d9dc02ae85e2
EC2 Default User <ec2-user@ip-172-31-12-26.ap-northeast-1.compute.internal>
2023-04-12 15:15:59 +0000

    課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   def get_profile_image(width, height)
@@ -15,6 +16,6 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+    profile_image.variant(resize_to_limit: [width, height])
   end
 end
