c706d3ba2f7079eafe5dfba6a882ccc5e24ede3f
EC2 Default User <ec2-user@ip-172-31-12-26.ap-northeast-1.compute.internal>
2023-04-15 06:50:59 +0000

    favorites, comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
   #resources :books
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy ] #do
-       #resource :favorites, only: [:create, :destroy]
-  #end
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy ] do
+       resource :favorites, only: [:create, :destroy]
+       resources :book_comments, only: [:create, :destroy]
+  end
 
   resources :users, only: [:index, :show, :edit, :update ]
 
